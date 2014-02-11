6e038780bd163eb5ab59723d5bc203e523ae89b9
Devendra Thakare <devendra@Devendras-MacBook-Pro.local>
2014-02-11 19:04:59 +0530

    added college markers

--- js/leads.js
+++ js/leads.js
@@ -10,35 +10,37 @@ var polygon_arr = [];
 var unique=1;
 var service = 'rent';
 var city_id = 1;
-
+var colleges_data = null;
+var city_map = {'1':'mumbai', '40':'delhi', '35':'gurgaon', '37':'pune', '75':'faridabad', '41':'ghaziabad', '39':'noida', '36':'hyderabad', '74':'chennai', '38':'bangalore', '76':'kolkata'}
+rich_marker_arr = [];
 var map_styles= [
-        {
-        featureType: "all",
-        elementType: "all",
-        stylers: [{saturation: -80}]
-        }
-    ,
-        {
-        featureType: "water",
+  {
+  featureType: "all",
+  elementType: "all",
+  stylers: [{saturation: -80}]
+  }
+,
+  {
+  featureType: "water",
 
-        elementType: "all",
+  elementType: "all",
 
-        stylers: [{lightness: -25}]
-        }
-    ,
-        {
-        featureType: "transit.line",
+  stylers: [{lightness: -25}]
+  }
+,
+  {
+  featureType: "transit.line",
 
-        stylers: [{visibility: "off"}]
-        }
-    ,   
-        {
-        featureType: 'administrative',
+  stylers: [{visibility: "off"}]
+  }
+,   
+  {
+  featureType: 'administrative',
 
-        stylers: [{visibility: "simplified"}]
-        }
-    ,{ featureType: "road", stylers: [ { visibility: "off" } ] }
-    ];
+  stylers: [{visibility: "simplified"}]
+  }
+,{ featureType: "road", stylers: [ { visibility: "off" } ] }
+];
 
 var map_options = {
   zoom: 11,
@@ -54,33 +56,35 @@ var map_options = {
   }
 };
 
+var marker_options = {
+  map: map
+}
+
+var circle_options = {
+  strokeColor: '#358EFB',
+  strokeOpacity: 0.9,
+  strokeWeight: 2,
+  fillColor: '#358EFB',
+  fillOpacity: 0.5,
+  radius: 2500
+};
+
 
 
 var autocomplete_options = {componentRestrictions: {country: "in"}};
-var autocomplete_input =document.getElementById('query');
+var autocomplete_input =document.getElementById('locality-filter');
+var map = null;
+var autocomplete = null; 
 
 function initialize() {
-  update_csv_link_url();
-  var map = new google.maps.Map(document.getElementById('map-canvas'), map_options);
-  var marker_options = {
-    map: map
-  }
-
-  var circle_options = {
-    strokeColor: '#358EFB',
-    strokeOpacity: 0.9,
-    strokeWeight: 2,
-    fillColor: '#358EFB',
-    fillOpacity: 0.5,
-    map: map,
-    radius: 2500
-  };
+  map = new google.maps.Map(document.getElementById('map-canvas'), map_options);
   map.setOptions({styles: map_styles});
 
-  var marker = new google.maps.Marker(marker_options);
+  // var marker = new google.maps.Marker(marker_options);
   var circle = new google.maps.Circle(circle_options);
+  circle.setOptions({map:map});
 
-  var autocomplete = new google.maps.places.Autocomplete(autocomplete_input, autocomplete_options);
+  autocomplete = new google.maps.places.Autocomplete(autocomplete_input, autocomplete_options);
   autocomplete.bindTo('bounds', map);
 
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
@@ -89,7 +93,6 @@ function initialize() {
       return;
     }
     lat_lng = place.geometry.location;
-    marker.setPosition(lat_lng);
     circle.setCenter(lat_lng);
     map.setZoom(14);
     map.setCenter(lat_lng);
@@ -106,47 +109,79 @@ function update_csv_link_url(){
 }
 
 $(document).ready(function(){
+
+  $(document).on('city:changed', function(e, city_id){
+    window.city_id = city_id;
+    map.setOptions({center:new google.maps.LatLng(lat[window.city_id],lon[window.city_id]), zoom:11});
+    $(document).trigger('show:colleges');
+  });
+
+  $(document).on('show:colleges', function(){
+    city_colleges = colleges_data[city_map[window.city_id]];
+    _.each(rich_marker_arr, function(r_marker){
+      r_marker.setMap(null);
+    });
+    rich_marker_arr.length = 0;
+    _.each(city_colleges, function(obj, key){
+      if(map){
+        rich_marker_options ={
+          position: new google.maps.LatLng(obj.lat, obj.lon),
+          flat: true,
+          map:map,
+          anchor: RichMarkerPosition.MIDDLE,
+          content: '<div class="rich-marker small"><span class="marker-ico glyphicon glyphicon-record"></span><span class="marker-wrap"><span class=" glyphicon glyphicon-map-marker"></span><span class="marker-text">'+key+'</span></span></div>'
+        };
+        rich_marker = new RichMarker(rich_marker_options);
+        rich_marker_arr.push(rich_marker)
+      }
+    });
+  });
+
   $('.city-menu .city').click(function(e){
     e.stopPropagation();
     e.preventDefault();
-    window.city_id = $(this).data('value');
     $('.city-menu .city').removeClass('active');
     $(this).addClass('active');
     $('#info .apartment_type_info, #info .price_info').html('');
     $('#info, .map-wrap').removeClass('animate');
     $('#message').hide();
-    update_csv_link_url();
-    initialize();
+    autocomplete_input.blur();    
+    setTimeout(function(){ autocomplete_input.value = ''; autocomplete_input.focus(); }, 100);
+    $(document).trigger('city:changed', [$(this).data('value')])
   });
 
-  $('.controls-wrap .control').click(function(e){
-    e.stopPropagation();
-    e.preventDefault();
-    window.service = $(this).data('type');
-    $('.controls-wrap .control').removeClass('active');
-    $(this).addClass('active');
-    $('#info, .map-wrap').removeClass('animate');
-    $('#message').hide();
-    update_csv_link_url();
-    initialize();
+  $.get('colleges.json')
+  .done(function(data){
+    colleges_data = JSON.parse(data);
+    $(document).trigger('show:colleges');
   });
 
-  $('#info').on('click', '.list-header', function(){
-      if(!$(this).closest('ul').hasClass('un_expandable')){
-        if($(this).closest('ul').hasClass('expanded'))
-            $(this).closest('ul').removeClass('expanded');
-        else{
-            $('.has-children').removeClass('expanded');
-            $(this).closest('ul').addClass('expanded');
-        }
-      }
-  });
-  tmp = ''
-  $(legends).each(function(i,legend){
-    wrap_div = $('<div/>')
-    .addClass('legend')
-    .append($('<span/>').addClass('color-wrap').css({'background-color': legend.color}))
-    .append($('<span/>').addClass('legend-txt').text(legend.text))
-    .appendTo('.legends-wrap');
-  });
+  // $('.controls-wrap .control').click(function(e){
+  //   e.stopPropagation();
+  //   e.preventDefault();
+  //   window.service = $(this).data('type');
+  //   $('.controls-wrap .control').removeClass('active');
+  //   $(this).addClass('active');
+  //   $('#info, .map-wrap').removeClass('animate');
+  //   $('#message').hide();
+  // });
+
+  // $('#info').on('click', '.list-header', function(){
+  //   if(!$(this).closest('ul').hasClass('un_expandable')){
+  //     if($(this).closest('ul').hasClass('expanded'))
+  //       $(this).closest('ul').removeClass('expanded');
+  //     else{
+  //       $('.has-children').removeClass('expanded');
+  //       $(this).closest('ul').addClass('expanded');
+  //     }
+  //   }
+  // });
+
+  // $(legends).each(function(i,legend){
+  //   wrap_div = $('<div/>')
+  //   .addClass('legend')
+  //   .append($('<span/>').addClass('color-wrap').css({'background-color': legend.color}))
+  //   .append($('<span/>').addClass('legend-txt').text(legend.text))
+  //   .appendTo('.legends-wrap');
+  // });
 });
