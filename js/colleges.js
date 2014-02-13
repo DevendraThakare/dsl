5525f593997da12bd84074932e16a7b3daf02ec6
Devendra Thakare <devendra@Devendras-MacBook-Pro.local>
2014-02-13 13:29:36 +0530

    added offices

--- js/colleges.js
+++ js/colleges.js
@@ -1,58 +1,17 @@
-var colleges_data = null;
+var college_office_data = null;
 var rich_marker_arr = [];
-// var circle_options = {
-//   strokeColor: '#000',
-//   strokeOpacity: 0.9,
-//   strokeWeight: 2,
-//   fillColor: '#000',
-//   fillOpacity: 0.2,
-//   radius: 2500
-// };
-
-// var autocomplete_options = {componentRestrictions: {country: "in"}};
-// var autocomplete_input =document.getElementById('locality-filter');
-// var circle = null;
-// var circle_marker = null;
-// var autocomplete = null; 
 
 function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), map_options);
   map.setOptions({styles: map_styles});
   draw(window.city_id);
   marker = new google.maps.Marker({map: map});
-  // circle = new google.maps.Circle(circle_options);
-
-  // autocomplete = new google.maps.places.Autocomplete(autocomplete_input, autocomplete_options);
-  // autocomplete.bindTo('bounds', map);
-  // circle_marker_options ={
-  //   flat: true,
-  //   ZIndex: 1050,
-  //   // draggable: true,
-  //   anchor: RichMarkerPosition.MIDDLE,
-  //   content: '<div class="circle-marker small"><span class="marker-ico glyphicon glyphicon-record"></span></div>'
-  // };
-  // circle_marker = new RichMarker(circle_marker_options);
-  // google.maps.event.addListener(circle_marker, 'position_changed', function() {
-  //   circle.setCenter(circle_marker.getPosition());
-  // });
-
-  // google.maps.event.addListener(autocomplete, 'place_changed', function() {
-  //   var place = autocomplete.getPlace();
-  //   if (!place.geometry) {
-  //     return;
-  //   }
-  //   lat_lng = place.geometry.location;
-  //   circle_marker.setOptions({position:lat_lng, map:map})
-  //   circle.setOptions({center:lat_lng, map:map});
-  //   map.setZoom(14);
-  //   map.setCenter(lat_lng);
-  // });
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
 
 function draw(city_id){
   destroy_polygons();
+  destroy_rich_markers();
+
   $.ajax({
     url:'http://analytics.housing.com/heatmap_lead_invent.php?city=1',
     type:'get',
@@ -60,7 +19,6 @@ function draw(city_id){
   })
   .done(function(data){
     data = $.parseJSON(data)
-    console.log(data);
     keys = Object.keys(data);
     $(keys).each(function(i,key){
         obj = data[key];
@@ -74,6 +32,110 @@ function draw(city_id){
     });
     $(document).trigger('data:fetched')
   });
+
+  $.get('http://analytics.housing.com/get_insti.php?city='+city_id)
+  .done(function(data){
+    if(typeof data == 'string')
+      college_office_data = JSON.parse(data);
+    else
+      college_office_data = data;
+    $(document).trigger('show:colleges');
+  });
+}
+
+function addPolygon(obj,locality_id){
+  polygon = obj['encoded_polygon'];
+  decodePolygonMK2(locality_id, polygon, obj['color'],obj['apartment_type_data'], obj['price_data'],obj['locality_name'],obj['ratio'],obj['status'],obj['lat'], obj['lon']).setMap(window.map);
+}
+
+decodePolygonMK2 = function(locality_id, t, c, apartment_type_data, price_data, locality, ratio, status, lat, lng) {
+  var e = t.length, i = 0, n = [], o = 0, a = 0, s = false, r = google.maps.LatLng;
+  while (i < e) {
+      var l, p = 0, h = 0;
+      do {
+          l = t.charCodeAt(i++) - 63;
+          h |= (l & 31) << p;
+          p += 5
+      } while (l >= 32);
+      a += h & 1 ? ~(h >> 1) : h >> 1;
+      if (a > 180 * 1e6 || a < -(180 * 1e6)) {
+          s = true
+      }
+      p = 0;
+      h = 0;
+      do {
+          l = t.charCodeAt(i++) - 63;
+          h |= (l & 31) << p;
+          p += 5
+      } while (l >= 32);
+      o += h & 1 ? ~(h >> 1) : h >> 1;
+      s = o > 90 * 1e6 || o < -(90 * 1e6) ? 1e7 : 1e6;
+      
+      n.push(new r(a * 10 / s,o * 10/ s))
+  }
+
+  poly = new google.maps.Polygon({
+    paths: n,
+    strokeColor: '#A0A0A0',
+    strokeWeight: 1,
+    fillColor: c,
+    fillOpacity: 0.8
+  });
+
+  google.maps.event.addListener(poly, 'mouseover', function(event) {
+    apartment_type_data_obj = $.parseJSON(apartment_type_data);
+    this.setOptions({fillOpacity : 1.0,  strokeColor: '#A0A0A0', strokeWeight:2});
+    tooltip_html = '<div class="tt-header">'+locality+'</div><div class="tt-content">';
+    if(apartment_type_data_obj!=null){
+      tooltip_html = tooltip_html + '<div class="tt-inventory"><span class="key">Total Inventory </span><span class="value">'+apartment_type_data_obj["inventory"]+'</span></div>';
+      if(apartment_type_data_obj["opencrf_leads"]!=null)
+        tooltip_html = tooltip_html + '<div class="tt-crf-leads"><span class="key">Open CRF Leads</span><span class="value">'+apartment_type_data_obj["opencrf_leads"]+'</span></div>';
+      if(apartment_type_data_obj["pyr_leads"]!=null)
+        tooltip_html = tooltip_html + '<div class="tt-pyr-leads"><span class="key">PYR Leads</span><span class="value">'+apartment_type_data_obj["pyr_leads"]+'</span></div>';
+    }
+    else{
+      tooltip_html = tooltip_html + '<div class="tt-inventory"><span class="key">Total Inventory </span><span class="value">'+0+'</span></div>';
+      tooltip_html = tooltip_html + '<div class="tt-crf-leads"><span class="key">Open CRF Leads</span><span class="value">'+0+'</span></div>';
+      tooltip_html = tooltip_html + '<div class="tt-pyr-leads"><span class="key">PYR Leads</span><span class="value">'+0+'</span></div>';
+    }
+    tooltip_html = tooltip_html + '</div>';
+    tooltip.show(tooltip_html);
+  });
+  google.maps.event.addListener(poly, 'mouseout', function(event){
+    tooltip.hide();
+    this.setOptions({fillOpacity : 0.8, strokeWeight:1});
+  });
+  poly['lat'] = lat;
+  poly['lng'] = lng;
+  polygon_arr.push(poly)
+  return poly;
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function destroy_rich_markers(){
+  _.each(rich_marker_arr, function(r_marker){
+    r_marker.setMap(null);
+  });
+  rich_marker_arr.length = 0;
+}
+
+function draw_rich_marker(obj,key){
+  rich_marker_options ={
+    position: new google.maps.LatLng(obj.lat, obj.lng),
+    flat: true,
+    map:map,
+    anchor: RichMarkerPosition.BOTTOM,
+    content: '<div class="rich-marker small"><span class="marker-ico glyphicon glyphicon-record"></span><span class="marker-wrap"><span class="glyphicon glyphicon-map-marker"></span><span class="marker-text">'+key+'</span></span></div>'
+  };
+  rich_marker = new RichMarker(rich_marker_options);
+  google.maps.event.addListener( rich_marker, 'mouseover', function(event) {
+    this.setOptions({ZIndex:1000})
+  });
+  google.maps.event.addListener( rich_marker, 'mouseout', function(event) {
+    this.setOptions({ZIndex:1});
+  });
+  rich_marker_arr.push(rich_marker);
 }
 
 $(document).ready(function(){
@@ -83,51 +145,20 @@ $(document).ready(function(){
     map.setOptions({center:new google.maps.LatLng(lat[window.city_id],lon[window.city_id]), zoom:11});
     $(document).trigger('show:colleges');
     draw(window.city_id);
-    // circle.setMap(null);
-    // circle_marker.setMap(null);
   });
 
   $(document).on('show:colleges', function(){
-    city_colleges = colleges_data[city_map[window.city_id]];
-    _.each(rich_marker_arr, function(r_marker){
-      r_marker.setMap(null);
-    });
-    rich_marker_arr.length = 0;
+    city_colleges = college_office_data['college'];
+    city_offices = college_office_data['office'];
     _.each(city_colleges, function(obj, key){
       if(map){
-        rich_marker_options ={
-          position: new google.maps.LatLng(obj.lat, obj.lon),
-          flat: true,
-          map:map,
-          anchor: RichMarkerPosition.BOTTOM,
-          content: '<div class="rich-marker small"><span class="marker-ico glyphicon glyphicon-record"></span><span class="marker-wrap"><span class="glyphicon glyphicon-map-marker"></span><span class="marker-text">'+key+'</span></span></div>'
-        };
-        rich_marker = new RichMarker(rich_marker_options);
-        google.maps.event.addListener( rich_marker, 'mouseover', function(event) {
-          this.setOptions({ZIndex:1000})
-        });
-        google.maps.event.addListener( rich_marker, 'mouseout', function(event) {
-          this.setOptions({ZIndex:1});
-        });
-        rich_marker_arr.push(rich_marker)
+        draw_rich_marker(obj,key)
       }
     });
-  });
-
-  $('.city-menu .city').click(function(e){
-    e.stopPropagation();
-    e.preventDefault();
-    // autocomplete_input.blur();    
-    // setTimeout(function(){ autocomplete_input.value = ''; autocomplete_input.focus(); }, 100);
-    $(document).trigger('city:changed', [$(this).data('value')])
-  });
-
-  $.get('colleges.json')
-  .done(function(data){
-    if(typeof data == 'string')
-      colleges_data = JSON.parse(data);
-    else
-      colleges_data = data;
-    $(document).trigger('show:colleges');
+    _.each(city_offices, function(obj, key){
+      if(map){
+        draw_rich_marker(obj,key)
+      }
+    });
   });
 });
--- js/main.js
+++ js/main.js
@@ -23,6 +23,161 @@ function draw(city_id,service){
   });
 }
 
+function addPolygon(obj,locality_id){
+  polygon = obj['encoded_polygon'];
+  decodePolygonMK2(locality_id, polygon,obj['color'],obj['apartment_type_data'], obj['price_data'],obj['locality_name'],obj['ratio'],obj['status'],obj['lat'], obj['lon']).setMap(window.map);
+}
+
+decodePolygonMK2 = function(locality_id, t,c,apartment_type_data, price_data, locality,ratio,status, lat, lng) {
+  var e = t.length, i = 0, n = [], o = 0, a = 0, s = false, r = google.maps.LatLng;
+  while (i < e) {
+      var l, p = 0, h = 0;
+      do {
+          l = t.charCodeAt(i++) - 63;
+          h |= (l & 31) << p;
+          p += 5
+      } while (l >= 32);
+      a += h & 1 ? ~(h >> 1) : h >> 1;
+      if (a > 180 * 1e6 || a < -(180 * 1e6)) {
+          s = true
+      }
+      p = 0;
+      h = 0;
+      do {
+          l = t.charCodeAt(i++) - 63;
+          h |= (l & 31) << p;
+          p += 5
+      } while (l >= 32);
+      o += h & 1 ? ~(h >> 1) : h >> 1;
+      s = o > 90 * 1e6 || o < -(90 * 1e6) ? 1e7 : 1e6;
+      
+      n.push(new r(a * 10 / s,o * 10/ s))
+  }
+
+  poly = new google.maps.Polygon({
+  paths: n,
+  strokeColor: '#A0A0A0',
+  strokeWeight: 1,
+  fillColor: c,
+  fillOpacity: 0.7,
+  });
+  google.maps.event.addListener(poly, 'click', function(event) {
+    window.polygon = poly;
+    latlng = event.latLng;
+    html = '';
+    apartment_type_data_obj = null;
+    price_data_obj = null;
+    window.locality_id = locality_id;
+    window.locality_name =locality;
+    window.marker.setPosition(latlng);
+    $('#info .locality-name').text(locality);
+    if(apartment_type_data)
+      apartment_type_data_obj = $.parseJSON(apartment_type_data);
+    if(price_data)
+      price_data_obj = $.parseJSON(price_data);
+    $('#info .message').removeClass('alert');
+    if(apartment_type_data_obj){
+        $('#info .apartment_type_info, #info .price_info').html('');
+        makeTree(apartment_type_data_obj);
+        $('#info .info-content').find('.apartment_type_info').html(html);
+        html='';
+        if(price_data_obj)
+          makeTree(price_data_obj);
+        $('#info .info-content').find('.price_info').html(html);
+        if(ratio < 0.8)
+          $('#message').text('Traffic Required').addClass('alert');
+        else if(ratio > 1.25)
+          $('#message').text('Inventory Required').addClass('alert');
+        else
+          $('#message').text('Awesome!').removeClass('alert');
+    }
+    else{
+      $('#message').text('Sorry, no inventory found!').addClass('alert');
+      $('#info .info-content').find('.apartment_type_info').html('<div class="message">Sorry, no inventory found!</div>');
+      $('#info .info-content').find('.price_info').html('<div class="message">Sorry, no inventory found!</div>');
+    }
+    $('#info, .map-wrap').addClass('animate');
+    $('#message').show();
+  });
+
+  google.maps.event.addListener(poly, 'mouseover', function(event) {
+  apartment_type_data_obj = $.parseJSON(apartment_type_data);
+    if(ratio < 0.8)
+      message = 'Traffic Required';
+    else if(ratio > 1.25)
+      message = 'Inventory Required';
+    else
+      message = 'Awesome!';
+    this.setOptions({fillOpacity : 1.0,  strokeColor: '#A0A0A0', strokeWeight:2});
+    tooltip_html = '<div class="tt-header">'+locality+'</div><div class="tt-content">';
+    tooltip_html = tooltip_html + '<div class="tt-message">'+message+'</div>';
+    if(apartment_type_data_obj!=null){
+      tooltip_html = tooltip_html + '<div class="tt-inventory"><span class="key">Total Inventory </span><span class="value">'+apartment_type_data_obj["inventory"]+'</span></div>';
+      if(apartment_type_data_obj["req.inventory"]!=null)
+        tooltip_html = tooltip_html + '<div class="tt-demand"><span class="key">Required Inventory </span><span class="value">'+apartment_type_data_obj["req.inventory"]+'</span></div>';
+    }
+    else{
+      tooltip_html = tooltip_html + '<div class="tt-inventory"><span class="key">Total Inventory </span><span class="value">'+0+'</span></div>';
+      tooltip_html = tooltip_html + '<div class="tt-demand"><span class="key">Required Inventory </span><span class="value">'+0+'</span></div>';
+    }
+    tooltip_html = tooltip_html + '</div>';
+    tooltip.show(tooltip_html);
+  });
+  google.maps.event.addListener(poly, 'mouseout', function(event){
+    tooltip.hide();
+    this.setOptions({fillOpacity : 0.7, strokeWeight:1});
+  });
+  poly['lat'] = lat;
+  poly['lng'] = lng;
+  polygon_arr.push(poly)
+  return poly;
+}
+
+function makeTree(obj){
+  keys = Object.keys(obj);
+  if(keys.length !=0){
+    $(keys).each(function(i, key){
+      if(key!='Total'){
+        if(typeof obj[key] === 'object'){
+          // $('<ul/>').addClass('has-children')
+          // .append($('<li/>').append('<h4/>').addClass('list-header').text(key).append(makeTree(obj[key])));
+          expand_class = '';
+          if(obj[key]['inventory']!=null || obj[key]['req.inventory']!=null)
+            expand_class = 'un_expandable';
+          html = html + '<ul class="has-children '+expand_class+'">';
+          html = html + '<li> <h4 class="list-header">'+key;
+          if(obj[key]['Total']!=null){
+            inv = obj[key]['Total']['inventory'];
+            dem = obj[key]['Total']['req.inventory'];
+            tmp_class='';
+            if(inv < dem)
+              tmp_class="alert";
+            html = html + '<span class="inv-dem-ratio '+tmp_class+'" data-inv="'+inv+'" data-dem="'+dem+'">'+inv+' / '+dem+'</span>';
+          }
+          if(obj[key]['inventory']!=null || obj[key]['req.inventory']!=null){
+            inv = obj[key]['inventory'];
+            dem = obj[key]['req.inventory'];
+            tmp_class='';
+            if(inv < dem)
+              tmp_class="alert";
+            html = html + '<span class="inv-dem-ratio '+tmp_class+'">'+inv+' / '+dem+'</span>';
+          }
+          html = html+'</h4>';
+          makeTree(obj[key]);
+          html = html + '</li></ul>';
+        }
+        else{
+          // $('<ul/>').addClass('is-child')
+          // .append($('<li/>').append('<span/>').addClass('label').text(key).append('<span/>').addClass('value').text(obj[key]));
+          // html = html + '<ul class="is-child '+key+'" data-value="'+obj[key]+'">';
+          // html = html + '<li> <span class="label">'+ key +'</span><span class="value">'+obj[key] +'</span></li></ul>';
+          return;
+        }
+      }
+    });
+  }
+}
+
 function initialize() {
     update_csv_link_url();
     map = new google.maps.Map(document.getElementById('map-canvas'), map_options);
@@ -66,4 +221,14 @@ $(document).ready(function(){
     $(document).trigger('service:changed', [$(this).data('type')]);
   });
 
+  $('#info').on('click', '.list-header', function(){
+    if(!$(this).closest('ul').hasClass('un_expandable')){
+      if($(this).closest('ul').hasClass('expanded'))
+          $(this).closest('ul').removeClass('expanded');
+      else{
+          $('.has-children').removeClass('expanded');
+          $(this).closest('ul').addClass('expanded');
+      }
+    }
+  });
 });
